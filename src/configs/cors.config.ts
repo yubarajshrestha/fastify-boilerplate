c95a9e9d8d4417ad98e0864585e55a905ca7197b
Yubaraj Shrestha <companion.krish@outlook.com>
2025-08-26 08:22:23 +0545

    updated cors configuration

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,23 +1,28 @@
 import cors from "@fastify/cors";
 import type { FastifyInstance } from "fastify";
 
-const allowedHosts = ["127.0.0.1:5173", "localhost:5173", "swaranga.com"];
-
-const corsOptionsDelegate = (req, callback) => {
-	const origin = req.headers.origin;
-	if (origin && allowedHosts.some((domain) => origin.includes(domain))) {
-		// If the origin is in the allowedHosts, allow it
-		callback(null, { origin: true });
-	} else {
-		// Deny the request from any other origin
-		callback(null, { origin: false });
-	}
-};
+const allowedOrigins = [
+	"http://127.0.0.1:5173",
+	"http://localhost:5173",
+	"http://0.0.0.0:5173",
+	"https://example.com",
+];
 
 const SetupCors = async (app: FastifyInstance) => {
 	await app.register(cors, {
-		delegator: corsOptionsDelegate,
-		methods: ["GET", "POST", "PUT", "DELETE"],
+		// delegator: corsDelegator,
+		origin: (origin, callback) => {
+			const checkedOrigin = origin || "";
+			if (!checkedOrigin) {
+				// Allow requests with no origin (like mobile apps or curl requests)
+				return callback(null, true);
+			}
+			const allowed =
+				allowedOrigins.some((allowedOrigin) => allowedOrigin.includes(checkedOrigin)) ||
+				process.env.NODE_ENV === "development";
+			callback(null, allowed);
+		},
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 		exposedHeaders: ["Content-Disposition"],
 		credentials: true,
